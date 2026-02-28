dd425dd5741a77535660d1750ab07cb48b4b81fa
gundamgn000 <gundamgn000@gmail.com>
2026-02-28 17:48:25 +0800

    add favicon

--- src/utils/shipping.js
+++ src/utils/shipping.js
@@ -7,7 +7,7 @@
  *    - 信用卡（宅配到府）= 150
  *    - 其他（超商取貨付款）= 80
  */
-export const OPENING_END = new Date("2026-02-26T23:59:59+08:00");
+export const OPENING_END = new Date("2026-02-28T23:59:59+08:00");
 
 export const calculateShipping = (paymentMethod, now = new Date()) => {
   if (now <= OPENING_END) return 0;
